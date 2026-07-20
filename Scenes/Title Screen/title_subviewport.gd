1960f835ceb4ac409ebd7b16f4f8cfc018ec0652
SkyeKyu <skye.micu@gmail.com>
2026-07-20 16:46:25 +0100

    Begining work on the Splash, Title and Main menu

---
+++ Scenes/Title Screen/title_subviewport.gd
@@ -0,0 +1,102 @@
+extends Node3D
+
+@onready var titles = [
+	$Chase,
+	$That,
+	$Score
+]
+
+@onready var splash_sprite = $SplashSprite
+
+@onready var music_stream_player = $MusicStreamPlayer
+@onready var sfx_stream_player = $SFXStreamPlayer
+
+# Load SFX.
+var sfx_audience_cheer = load("res://Assets/Audio/SFX/audience-cheer.mp3")
+var sfx_audience_chatter = load("res://Assets/Audio/SFX/crowd-chatter.mp3")
+
+# Set where we want the title parts to end up.
+var current_title_part := 0
+
+var target_positions = [
+	Vector3(-0.48, 0.37, -1.68),Vector3(0, 0.37, -1.68),Vector3(0.47, 0.37, -1.68)
+]
+var target_rotations = [
+	Vector3(0,0,0),	Vector3(0,0,26.0),	Vector3(0,0,0),
+]
+
+# The speed with which the title parts zoom to the viewport.
+var speed := 24.0
+
+var time := 0.0
+var start_title_animation := false
+
+func _ready():
+	for title in titles:
+		title.position = Vector3(0, 0, -20)
+		title.visible = false
+	fade_out_and_remove()
+	start_sequence()
+
+func start_sequence():
+	sfx_stream_player.stream = sfx_audience_chatter
+	sfx_stream_player.play()
+	await sfx_stream_player.finished
+	sfx_stream_player.stream = sfx_audience_cheer
+	music_stream_player.play()
+	await get_tree().create_timer(2).timeout
+	sfx_stream_player.play()
+	await get_tree().create_timer(6).timeout
+	start_title_animation = true
+
+func fade_out_and_remove():
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(splash_sprite, "modulate:a", 0.0, 3.0)
+	tween.tween_callback(splash_sprite.queue_free)
+
+func _process(delta):
+	if not start_title_animation:
+		return
+	
+	time += delta
+
+	# Bring words in one at a time.
+	if current_title_part < titles.size():
+		var title = titles[current_title_part]
+		var target_position = target_positions[current_title_part]
+		var target_rotation = target_rotations[current_title_part]
+
+		if not title.visible:
+			title.visible = true
+		
+		title.position = title.position.move_toward(
+			target_position,
+			delta * speed
+		)
+		
+		title.rotation = title.rotation.move_toward(
+			target_rotation,
+			delta * speed
+		)
+
+		if title.position.distance_to(target_position) < 0.05 && title.rotation.distance_to(target_rotation) < 0.05:
+			current_title_part += 1
+
+	# Idle animation once everything has arrived.
+	else:
+		for i in titles.size():
+			var title = titles[i]
+			var target_position = target_positions[i]
+			var target_rotation = target_rotations[i]
+
+			var offset = i * 0.4
+
+			title.position.y = target_position.y + sin(time * 1.2 + offset) * 0.03
+
+			title.rotation_degrees.z = sin(time * 0.8 + offset) * 0.1 + target_rotation.z
+
+			var scale_amount = 1.0 + sin(time * 1.5 + offset) * 0.02
+
+			title.scale = Vector3.ONE * scale_amount
