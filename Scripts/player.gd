c361f1510e5a6b915adc90c13e02231be35bc00f
NebulaCoffee <119163054+NebulaCoffee@users.noreply.github.com>
2026-07-21 12:21:05 +0100

    player character stuff
    Added input controls and placeholder animations to the player character

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,28 @@
 extends Node2D
 
-@export var speed: int
+# Variables
+@export var speed: int # Determines the current speed of the character
+
+# Nodes
+@onready var player_ani: AnimatedSprite2D = $PlayerAni
+@onready var player_char: CharacterBody2D = $PlayerChar
+@onready var player_col: CollisionShape2D = $PlayerChar/PlayerCol
 
 func _ready() -> void:
 	speed = 0
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_right"):
+		_set_ani("running",1)
+	if Input.is_action_just_pressed("ui_left"):
+		_set_ani("slowing",-1)
+	if Input.is_action_just_pressed("ui_up"):
+		_set_ani("jump",0)
+	if Input.is_action_just_pressed("ui_down"):
+		_set_ani("skid",0)
+
+# Function for changing the player speed and current animation
+func _set_ani(ani : String, spd : int)->void:
+	speed += spd
+	print(speed)
+	player_ani.animation = ani
