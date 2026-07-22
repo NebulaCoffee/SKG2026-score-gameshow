fb56d20c835dfced6b19fff7a609c42533ce923a
Brook Zafar <16zafara@gmail.com>
2026-07-22 23:21:53 +0100

    Added script to load scene into another scene to form the level as the player runs. Currently not working.

--- Scripts/load_next_chunk.gd
+++ Scripts/load_next_chunk.gd
@@ -3,7 +3,12 @@ extends Area3D
 const test_chunk = preload("res://Scenes/LevelChunks/test_chunk.tscn")
 var next_chunk
 
+
+func _ready() -> void:
+	pass
+
 func _on_body_entered(body: Node3D) -> void:
 	next_chunk = test_chunk.instantiate()
-	add_child(next_chunk)
-	#next_chunk.position = Vector3(50,0,0)
+	get_parent().add_child(next_chunk)
+	next_chunk.position = Vector3(170,0,0)
+	print(next_chunk.get_child(0).global_position)
